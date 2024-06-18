38ba619a9adc18a9d1180d837fbdccf09acc7842
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2024-06-17 22:50:31 -0300

    Start Project

---
+++ src/main/java/br/com/inventorycontrol/configuration/controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package br.com.inventorycontrol.configuration.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/teste")
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String>  helloWorld(){
+        return ResponseEntity.ok().body("Hello World");
+    }
+}
---
+++ src/test/java/br/com/inventorycontrol/InventoryControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inventorycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
